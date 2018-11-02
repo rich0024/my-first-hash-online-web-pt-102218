fa586f920d5c2f692ae701a9dc01b3f2fad48af8
Richard Yance <yance0022@gmail.com>
2018-11-02 15:10:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,8 +17,8 @@ end
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
+                  "oil paintings" => 3,
+                  "porcelain vases" => 2
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
