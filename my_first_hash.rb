78474109458388900f478bd682d80002dc725c0a
Richard Yance <yance0022@gmail.com>
2018-11-02 15:09:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,6 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-retrieval[shipping_manifest["oil paintings"]]
 end
 
 def adding
